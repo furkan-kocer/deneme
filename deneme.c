ee55e566e785c643a300c4f655d490a20792bd97
furkan <furkan.1732@hotmail.com>
2021-08-12 00:19:52 +0300

    files written by furkan

---
+++ deneme.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    int x=0;
+for(int i=0;i<8;i++)
+{
+    x=x+i;//basit 0dan baþlayýp 8e kadar olan sayýlarýn toplamý örneði
+
+}
+printf("x degeri:%d",x);
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Deneme icindeki kodun amaci 0 dan baslayip 8 e kadar olan sayıların toplamını veren cikti.
